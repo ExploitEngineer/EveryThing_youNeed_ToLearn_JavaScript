1a61606909ce7a146daf6cae1319f9c834579e4d
DSnake0 <revilwhoami0098@gmail.com>
2025-01-26 12:59:53 +0500

    Add new line in part1/opePre.js file

--- part1/opePre.js
+++ part1/opePre.js
@@ -1,3 +1,4 @@
 let score = 2 * 3 + 2 - 1; // don't do like that
-let score1 = ((2 * (3 + 2)) - 1); // instead do like this 
+let score1 = ((2 * (3 + 2)) - 1); // instead do like this
 console.log(score);
+console.log(score1);
