621a355a559b4d167afadd1df8f71ee86fb7a100
ExploitEngineer <revilwhoami0098@gmail.com>
2025-02-11 18:57:19 +0500

    Add new file math.js in part1 folder

---
+++ part1/math.js
@@ -0,0 +1,32 @@
+// INFO: Math is JavaScript
+
+// NOTE: Mathematical Operators
+/*
+1. Addition Operator: +
+2. Subtraction Operator: -
+3. Multiplication Operator: *
+4. Division Operator: /
+5. Remainder Operator: %
+6. Exponentiation Operator: **
+*/
+
+console.log(1 + 1);
+console.log(2 - 2);
+console.log(2 * 6);
+console.log(2 / 6);
+console.log(2 % 4);
+console.log(2 ** 4);
+
+
+// Properties of Math Object
+console.log(Math.PI);
+
+// Methods of Math Object
+console.log(Math.sqrt(16));
+console.log(Math.floor(2.99));
+console.log(Math.floor(-2.5));
+console.log(Math.ceil(2.1));
+console.log(Math.ceil(-2.5));
+console.log(Math.round(2.5));
+console.log((Math.random()));
+console.log(Math.round(Math.random() * 20));
