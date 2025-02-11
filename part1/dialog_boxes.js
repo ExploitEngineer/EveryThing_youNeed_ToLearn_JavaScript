8f6d8157e1c30a558402a70bc940bb83814081eb
ExploitEngineer <revilwhoami0098@gmail.com>
2025-02-11 17:03:58 +0500

    Add new file dialog_boxes.js in part1 folder

---
+++ part1/dialog_boxes.js
@@ -0,0 +1,10 @@
+// There are three types of Dialog Boxes in Javascript
+
+// INFO:  Display a box with okay button
+alert("Hello i am here");
+
+// INFO: Display a box with okay and cancel button and input field
+prompt("hi how's your day going");
+
+//  INFO: Display a box with okay and cancel button
+confirm("hi");
