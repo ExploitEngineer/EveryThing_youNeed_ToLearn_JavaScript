af993f9ded246b8ed578245d071658cf17c82aea
ExploitEngineer <revilwhoami0098@gmail.com>
2025-02-12 19:17:31 +0500

    Add new file multidimensional_array.js file in part4 folder

---
+++ part4/multidimensional_array.js
@@ -0,0 +1,6 @@
+// INFO: Multidimensional Arrays
+// Multidimensional arrays are also called arrays inside array.
+
+const arr = [['zain', 20], ['rafay', 10]];
+console.log(arr[0][0]);
+console.log(arr[1][0]);
