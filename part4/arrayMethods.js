660b5c10c7fa990a39f7f616e176a3f4581943e1
ExploitEngineer <revilwhoami0098@gmail.com>
2025-02-12 19:03:36 +0500

    Add new array methods in arrayMethods.js

--- part4/arrayMethods.js
+++ part4/arrayMethods.js
@@ -8,7 +8,7 @@ console.log(arr1.toString());
 
 // arrayat() Method
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
-console.log(fruits.at(-1));
+console.log(fruits.at(0));
 
 // Array.join() Method
 const arr2 = [1, 2, 3, 4, 5];
@@ -36,7 +36,7 @@ const arr7 = [1, 2, 3, 4, 5];
 arr7.unshift(0);
 console.log(arr7);
 
-// Array delete Method
+// Array delete Method // NOTE: don't use this delete method cause this will return undefined
 const arr8 = [1, 2, 3, 4, 5, 6];
 delete arr8[2];
 console.log(arr8);
@@ -57,6 +57,28 @@ const arr12 = [1, 2, 3, 4, 5, [6, 7], [8, 9, [10]]];
 const flatArray = arr12.flat(2);
 console.log(flatArray);
 
+// Array tospliced method
+const arr15 = [1, 2, 6, 4, 5];
+const newarr15 = arr15.toSpliced(2, 1, 3);
+console.log(arr15);
+console.log(newarr15);
+
+// Array indexOf method
+const arr16 = [1, 2, "whoami"];
+console.log(arr16.indexOf("whoami"))
+
+// Array includes method
+const arr17 = [1, 2, 3, 4, "elements"];
+console.log(arr17.includes("elements"));
+
+// Array reverse method
+const arr18 = [1, 2, 3, 4, 5, 6];
+console.log(arr18.reverse());
+
+// Array sort method
+const arr19 = [1, 2234, 2, 324, 234242, 23423, 2343, 34];
+console.log(arr19.sort());
+
 // Array slice Method
 const arr13 = [1, 2, 3, 4, 5];
 const newarr13 = arr13.slice(1, 3);
@@ -67,9 +89,3 @@ const arr14 = [1, 2, 3, 4, 5];
 arr14.splice(2, 0, 6, 7);
 arr14.splice(2, 1, 8, 9);
 console.log(arr14);
-
-// Array tospliced method
-const arr15 = [1, 2, 6, 4, 5];
-const newarr15 = arr15.toSpliced(2, 1, 3);
-console.log(arr15);
-console.log(newarr15);
