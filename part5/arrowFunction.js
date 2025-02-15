13191309634fca5dc1d8552bffdffa39c2367fdb
ExploitEngineer <revilwhoami0098@gmail.com>
2025-02-15 21:59:31 +0500

    Add new file arrowFunction.js file in part5 folder

---
+++ part5/arrowFunction.js
@@ -0,0 +1,27 @@
+// Function Declaration
+/*
+function square(num) {
+    return num * num;
+}
+*/
+
+// Function Expression
+/*
+const square = function (num) {
+    return num * num;
+};
+*/
+
+// Arrow Function Expression
+/* const square = (num) => {
+    return num * num;
+} */
+
+const square = (num) => num * num;
+const add = (a, b) => a + b;
+
+const random = () => Math.random()
+
+setTimeout(() => {
+    console.log("hi");
+}, 2000);
