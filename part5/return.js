2cafe35e58dc170396e88b49580d3d663acdc968
ExploitEngineer <revilwhoami0098@gmail.com>
2025-02-15 10:15:59 +0500

    Add new file return.js file in part5 folder

---
+++ part5/return.js
@@ -0,0 +1,8 @@
+// INFO: Return Keyword in JavaScript Functions ....
+
+function addTwoNumbers(a, b) {
+    console.log(`The sum of ${a} & ${b} is ${a + b}`);
+    return a + b;
+};
+const result = addTwoNumbers(5, 6);
+console.log(result);
