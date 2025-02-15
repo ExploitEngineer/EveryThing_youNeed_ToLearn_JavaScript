73f4abfb5e2cc781d1b0669d9e76b92b06ed8d8d
ExploitEngineer <revilwhoami0098@gmail.com>
2025-02-15 22:42:59 +0500

    Add new file forloop.js file in part4 folder

---
+++ part4/forloop.js
@@ -0,0 +1,22 @@
+// INFO: For Loop
+
+console.log("Program Started");
+
+for (let i = 1; i < 11; i++) {
+    console.log(`${i} I Like for loop so much ....`);
+}
+
+console.log("Program Ended");
+
+
+for (let i = 0; i < 102; i += 2) {
+    console.log(`Print Even numbers ${i}`)
+}
+
+for (let i = 0; i < 100; i++) {
+    if (i % 2 === 0){
+        console.log(`Event numbers ${i}`);
+    }else {
+        console.log(`Odd numbers ${i}`);
+    }
+}
