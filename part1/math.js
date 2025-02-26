d746a23d77792e7f1f840eb3f6699c51ed396c85
whoami <revilwhoami0098@gmail.com>
2025-02-26 18:57:59 +0500

    Update math.js

--- part1/math.js
+++ part1/math.js
@@ -15,18 +15,21 @@ console.log(2 - 2);
 console.log(2 * 6);
 console.log(2 / 6);
 console.log(2 % 4);
-console.log(2 ** 4);
 
+// this two works same but the recommendation is the first one 
+console.log(2 ** 4);
+console.log(Math.pow(2, 4)); 
 
 // Properties of Math Object
 console.log(Math.PI);
 
 // Methods of Math Object
-console.log(Math.sqrt(16));
-console.log(Math.floor(2.99));
-console.log(Math.floor(-2.5));
-console.log(Math.ceil(2.1));
-console.log(Math.ceil(-2.5));
-console.log(Math.round(2.5));
-console.log((Math.random()));
-console.log(Math.round(Math.random() * 20));
+console.log(Math.sqrt(16)); // 4
+console.log(Math.floor(2.99)); // 2
+console.log(Math.floor(-2.5)); // -3
+console.log(Math.ceil(2.1)); // 3
+console.log(Math.ceil(-2.5)); // -2
+console.log(Math.round(2.5)); // 3
+console.log(Math.round(2.4)) // 2
+console.log((Math.random())); // any random value 
+console.log(Math.round(Math.random() * 20)); // gives the random value 1 to 20
