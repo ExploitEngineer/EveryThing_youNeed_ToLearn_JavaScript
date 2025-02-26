50174ab49b99e8b4be13e942712bd056b4ee2f7f
whoami <revilwhoami0098@gmail.com>
2025-02-27 00:01:17 +0500

    Update forloop.js

--- part4/forloop.js
+++ part4/forloop.js
@@ -20,3 +20,38 @@ for (let i = 0; i < 100; i++) {
         console.log(`Odd numbers ${i}`);
     }
 }
+
+
+
+// NOTE: Quizses for (for loops)
+
+// INFO: First Quiz 
+for (let i = 1; i <= 3; i++) {
+    console.log(i);
+} // Answer : 1 2 3
+
+
+// INFO: Second Quiz
+for (let i = 5; i > 0; i--) {
+    console.log(i);
+} // Answer : 5 4 3 2 1
+
+
+// INFO: Third Quiz 
+for (let i = 1; i <= 10; i += 2) {
+    console.log(i);
+} // Answer : 1 3 5 7 9
+
+
+// INFO: Fourth Quiz 
+for (let i = 1; i <= 5; i++) {
+    if (i === 3) break;
+    console.log(i);
+} // Answer : 1 2
+
+
+// INFO: Fifth Quiz 
+for (let i = 1; i <= 5; i++) {
+    if (i === 3) continue; 
+    console.log(i);
+} // Answer : 1 2 3 4 5
