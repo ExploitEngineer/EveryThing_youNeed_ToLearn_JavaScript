e89482e58b08540dd0db198152b7e4864d822646
whoami <revilwhoami0098@gmail.com>
2025-02-26 23:29:14 +0500

    Update do_whileloop.js

--- part4/do_whileloop.js
+++ part4/do_whileloop.js
@@ -10,3 +10,52 @@ do {
 
 
 console.log("Program Ended");
+
+
+
+// NOTE: Do while loop quizses
+
+//INFO: First Quiz
+let i = 5; 
+do {
+    console.log(i);
+    i--;
+} while (i > 3 );  // Answer: 5 4
+
+
+//INFO: Second Quiz 
+let i = 10;
+do {
+    console.log(i);
+    i++;
+} while(i < 10); // Answer: 10
+
+
+//INFO: Third Quiz 
+let num = 1;
+do {
+    console.log(num);
+} while (num < 5); // Answer: infinite Loop
+
+
+//INFO: Fourth Quiz 
+let count = 3;
+do {
+    console.log(count);
+    count--;
+} while(count >= 0); // Answer: 3 2 1 0
+
+
+// INFO: Fifth Quiz 
+let x = 7;
+do {
+    console.log("Hello");
+} while (x < 5); // Answer: Hello
+
+
+// INFO: Sixth Quiz 
+let num = 2;
+do {
+    console.log(num);
+    num += 2;
+} while (num <= 10); // Answer: 2 4 6 8 10
