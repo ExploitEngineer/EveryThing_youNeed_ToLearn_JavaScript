7be8df30092a120b575ae4c36d3c086a96fd59d4
whoami <revilwhoami0098@gmail.com>
2025-02-26 19:15:49 +0500

    Update restParameter.js

--- part1/restParameter.js
+++ part1/restParameter.js
@@ -1,6 +1,42 @@
-const nums1 = [1, 2, 3, 4];
+// Rest parameters 
 
-function add(...nums) {
-    console.log(nums);
+function openFridge(...foods) {
+    console.log(...foods);
 }
-add(1, 2, 3);
+
+function getFood(...foods) {
+    return foods;
+}
+
+const food1 = "pizza";
+const food2 = "hamburger";
+const food3 = "hotdog";
+const food4 = "sushi";
+
+openFridge(food1, food2, food3, food4);
+
+const foods = getFood(food1, food2, food3, food4);
+
+
+
+
+function sum(...numbers){
+    let result = 0;
+    for(let number of numbers){
+        result += number;
+    }
+    return result;
+}
+
+const total = sum(1);
+
+console.log(`Your total is $${total}`);
+
+
+
+
+function combineString(...strings) {
+    return strings.join(" ");
+}
+const fullName = combineString("Mr.", "spongebob", "Squarepant", "III");
+console.log(fullName);
