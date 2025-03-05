266f12d9c2e6f6978f1b8a8e28b1bbb641d05767
whoami <revilwhoami0098@gmail.com>
2025-03-05 19:24:34 +0500

    Update promises.js

--- part9_Advanced/promises.js
+++ part9_Advanced/promises.js
@@ -78,6 +78,24 @@ p.then(() => {
 })
 
 
+// Best way to handle api using promises 
+function fetchData(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => resolve(data))
+            .catch(error => reject(error));
+    });
+}
+
+fetchData("https://fakestoreapi.com/products")
+    .then(data => console.log(data))
+    .catch(error => console.error("Error:", error));
+
+
+
+
+
 // NOTE: Promises Quizes 
 
 // INFO: First Quiz 
