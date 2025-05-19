6461f592a4a3111f9cb1ab6999a3f3f569ac4cd0
ExploitEngineer <revilwhoami0098@gmail.com>
2025-05-19 16:57:41 +0500

    feat: add new changes in try catch file

--- part9 (Advanced Topics)/08_try&catch.js
+++ part9 (Advanced Topics)/08_try&catch.js
@@ -1,29 +1,59 @@
+// What is try & catch ?
+// The try & catch blocks lets you run code that might throw an error, and then handle that error in a clean way.
+
+// Basic Syntax
 try {
-    console.log(a);
+  // Code that might throw an error
+} catch (error) {
+  // Code to handle the error
+}
+
+// Exmaples
+try {
+  let user = JSON.parse('{name: "John"}'); // invalid json
+  console.log(user.name);
+} catch (error) {
+  console.log("Error caught!");
+  console.log(err.message);
+}
+
+try {
+  let num = 10;
+  let result = num.toUpperCase(); // error
+} catch (e) {
+  console.log("Caught:", e.message);
+} finally {
+  console.log("This always runs!");
+}
+
+try {
+  console.log(a);
 } catch (err) {
-    console.log(err);
+  console.log(err);
 }
 
 console.log("Hello WOrld");
 
-fetch('https://dummyjson.com/products').then((res) => {
+fetch("https://dummyjson.com/products")
+  .then((res) => {
     return console.log(res.json());
-}).then((data) => {
+  })
+  .then((data) => {
     console.log(data);
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-})
-
+  });
 
 async function makeAsyncRequest() {
-    try {
-        const url = 'https://dummyjson.com/products';
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-        return data;
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const url = "https://dummyjson.com/products";
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 }
 makeAsyncRequest();
