91fd1d256d4bb8c79d8ff88b2ecf57f2f9caf1c8
ExploitEngineer <revilwhoami0098@gmail.com>
2025-06-09 06:35:33 +0500

    feat: add comma operators

---
+++ part1 (Basics)/19_comma.js
@@ -0,0 +1,13 @@
+// Comma Operator in JavaScript
+
+/*
+,    Comma operator
+Evaluates multiple expressions and returns the value of the last one.
+*/
+
+let x = (1, 2, 3);
+console.log(x); // Output: 3
+
+let y;
+y = (console.log('First'), console.log('Second'), 42);
+console.log(y); // Output: 42
