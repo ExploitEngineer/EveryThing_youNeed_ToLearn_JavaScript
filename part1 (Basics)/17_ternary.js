be42de49e72000989f53c79d1b0540d5d2cb100a
ExploitEngineer <revilwhoami0098@gmail.com>
2025-06-09 06:34:14 +0500

    feat: add ternary operator

---
+++ part1 (Basics)/17_ternary.js
@@ -0,0 +1,11 @@
+// Ternary Operator in JavaScript
+
+/*
+? :   Ternary conditional operator
+Syntax: condition ? expressionIfTrue : expressionIfFalse
+Returns one of two expressions based on a condition.
+*/
+
+const age = 18;
+const canVote = age >= 18 ? 'Yes' : 'No';
+console.log(canVote); // Output: Yes
