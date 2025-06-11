9e5e83804de466916f7bfe3ddbaac6a308f57469
ExploitEngineer <revilwhoami0098@gmail.com>
2025-06-12 00:00:44 +0500

    feat: add difference between properties and methods

---
+++ part1 (Basics)/09_methods_vs_properties.js
@@ -0,0 +1,10 @@
+// INFO: Difference between Properties and Methods in JavaScript 
+
+
+// Properties are static values tied to a string (e.g., .length). You access them - you don't call them.
+
+
+// Methods are functions that you call on strings to perform actions (e.g., .toUpperCase()).
+
+
+
