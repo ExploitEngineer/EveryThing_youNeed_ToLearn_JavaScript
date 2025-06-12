79511cc39ddfbf8f6ceaba0b8b874f6a91e05bd3
ExploitEngineer <revilwhoami0098@gmail.com>
2025-06-12 20:51:11 +0500

    feat: add function declaration

---
+++ part5 (Functions)/AdvanceFunctions/01_function.declaration.js
@@ -0,0 +1,15 @@
+/*
+INFO: What is Function Declaration ? 
+A function declaration (also called a "function statement") is one of the most common ways to define a function in javascript. It creates a named function using the function keyword.
+*/
+
+// Syntax 
+function functionName() {
+  console.log("hi");
+}
+
+// Example 
+function greet(name) {
+  console.log(`Hello ${name}!`);
+}
+greet("Rafay");
