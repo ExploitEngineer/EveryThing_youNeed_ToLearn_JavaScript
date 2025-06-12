e2d229b9ce4a71f397c275076218cc7ccef7fc9d
ExploitEngineer <revilwhoami0098@gmail.com>
2025-06-12 20:34:14 +0500

    feat: add comments in javascript

---
+++ part1 (Basics)/10_comments.js
@@ -0,0 +1,22 @@
+/*
+INFO: What are Comments ?
+Comments are lines in your code that are ignored by the javascript engine. They're used to explain, organize, or disable parts of your code temporarily.
+*/
+
+/*
+INFO: Signle-line Comment
+Use // to comment a single line.
+*/
+
+// let name = "Rafay"; 
+
+
+/*
+INFO: Multi-line Comment
+Use /* * / to comment across multiple lines.
+*/
+
+/*
+let name = "rafay";
+let number = 123;
+*/
