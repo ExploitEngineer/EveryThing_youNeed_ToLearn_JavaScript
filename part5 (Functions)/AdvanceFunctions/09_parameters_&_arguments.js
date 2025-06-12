6d5afb7f17bc6a2a8ca923f11f6b015ec78db031
ExploitEngineer <revilwhoami0098@gmail.com>
2025-06-12 21:24:41 +0500

    feat: add parameters & arguments

---
+++ part5 (Functions)/AdvanceFunctions/09_parameters_&_arguments.js
@@ -0,0 +1,11 @@
+/*
+INFO: What are parameters and arguments ? 
+1. Parameters: The variable listed is the function definition.
+2. Arguments: The actual values passed to the function when calling it.
+*/
+
+// Example 
+function greet(name) { // name is a parameter
+  console.log(`Hello, ${name}`);
+}
+greet("Rafay"); // "Rafay" is an argument
