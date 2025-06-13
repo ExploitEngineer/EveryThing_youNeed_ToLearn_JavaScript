9a66514fd99f8e5a70400126c250ea03ad26836e
ExploitEngineer <revilwhoami0098@gmail.com>
2025-06-13 16:56:53 +0500

    feat: add global object

---
+++ part9 (Advanced Topics)/21_global.object.js
@@ -0,0 +1,11 @@
+/*
+INFO: What is the Global Object ?
+The global object is the top-level object that provides global variables and functions.
+It behaves differently depending on where your javascript is running.
+
+
+INFO: Global Object in Different Environments
+Browser = window
+Node.js = global
+Universal (ES2020+) = globalThis
+*/
