e10e04c7abab3f5ec10ae7f2fb6ae36951814acd
ExploitEngineer <revilwhoami0098@gmail.com>
2025-06-13 16:53:45 +0500

    feat: add node.js vs browser

---
+++ part9 (Advanced Topics)/20_nodejs_vs_browser.js
@@ -0,0 +1,17 @@
+/*
+INFO: Browser (Client-Side JavaScript)
+JavaScript running in the browser, mainly used to build UI, interact with the DOM, and handle user events.
+1. Executes in Chrome, Firefox, Safari, etc.
+2. Can directly interact with HTML, CSS.
+3. Limited access to OS-level resources (for security)
+*/
+
+/*
+INFO: Node.js (Server-Side JavaScript)
+A runtime environment that allows javascript to run outside the browser, commonly on the server.
+1. Built on Chrome's V8 engine.
+2. Enables file system access, networking, database, and more.
+3. No access to DOM or window
+*/
+
+
