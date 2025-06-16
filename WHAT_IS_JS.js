dfc9be72cfde745cdc8f4f97382e61a2b252830e
ExploitEngineer <revilwhoami0098@gmail.com>
2025-06-16 05:28:56 +0500

    feat: add WHAT_IS_JS.js file

---
+++ WHAT_IS_JS.js
@@ -0,0 +1,15 @@
+/*
+INFO: About JavaScript
+JavaScript is a programming language that allows web pages to be dynamic. It is an interpreted language, which means that it doesn't need to be compiled: instead the interpreter (such as a browser) will parse the code and turn it into code that their machine can run - suitable for creating dynamic websites that can run on any browser on any computer!
+
+JavaScript is not only used in the browser. JavaScript runtimes, such as Node.js and Deno allow you to write, launch and serve requests on webservers. Other frameworks, such as Electron use JavaScript to write cross-platform applications for Windows, Linux and macOS. Mobile app development is also a posibility, utilising React Native, Ionic and various others, with Expo now allowing to target Andriod, IOS and the web, all at once.
+
+ECMAScript is the standard that defines JavaScript.
+    "ECMAScript has grown to be one of the world’s most widely used general-purpose programming languages. It is best known as the language embedded in web browsers but has also been widely adopted for server and embedded applications." — ECMA International Language Specification
+
+Starting with the 6th edition (commonly known as ES2015 or ES6) in 2015, a new edition of the standard will be released each year. The 6th edition was a major update that brought many enhancements over ES5, including notably template strings, expressive arrow function syntax, and cleaner syntax for defining classes.
+
+But because new syntax and features are coming to JavaScript each year, support for these changes is often incomplete in current browsers and the latest Node.js. This doesn't mean we can't use it. Tools such as Babel offer transpilation for most features, allowing us to write as if it's the future.
+
+Note: This track supports the latest ECMAScript syntax via Babel and the @babel/preset-env plugin, and new experimental features are enabled with each update of that plugin, matching the release of the specifications. It automatically adapts to your local Node.js installation. This means you don't need to worry about what is and isn't supported.
+*/
