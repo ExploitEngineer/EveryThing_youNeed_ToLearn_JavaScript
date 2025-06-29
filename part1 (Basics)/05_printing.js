2c72ba263fb66a7e407a8e7f4f2f3b5fbceca3b0
ExploitEngineer <revilwhoami0098@gmail.com>
2025-06-29 08:16:27 +0500

    feat: update printing file
    update the code for better understanding

--- part1 (Basics)/05_printing.js
+++ part1 (Basics)/05_printing.js
@@ -1,29 +1,22 @@
-// printing.js
-
 // INFO: Different Ways to Print Output in JavaScript
 
-// 1. console.log()
-//    - Prints messages to the console with a newline.
+// 1. console.log() - Prints messages to the console with a newline.
 console.log("Hello");
 
-// 2. process.stdout.write()
-//    - Writes output without adding a newline (Node.js specific).
+// 2. process.stdout.write() - Writes output without adding a newline (Node.js specific).
 process.stdout.write("hey\n"); // Added \n for newline
 
-// 3. console.table()
-//    - Displays objects or arrays in a table format for better readability.
+// 3. console.table() - Displays objects or arrays in a table format for better readability.
 console.table({ city: "Islamabad" });
 
-// 4. console.warn()
-//    - Prints warning messages, usually highlighted in the console.
+// 4. console.warn() - Prints warning messages, usually highlighted in the console.
 console.warn({ city: "newCity" });
 
-// 5. console.error()
-//    - Prints error messages, usually highlighted in the console
+// 5. console.error() - Prints error messages, usually highlighted in the console
 console.error("The error message");
 
 /*
 Note:
-- In browsers, `console.log()`, `console.warn()`, and `console.table()` are commonly used.
-- `process.stdout.write()` is primarily used in Node.js environments.
+- In browsers, console.log(), console.warn(), and console.table() are commonly used.
+- process.stdout.write() is primarily used in Node.js environments.
 */
