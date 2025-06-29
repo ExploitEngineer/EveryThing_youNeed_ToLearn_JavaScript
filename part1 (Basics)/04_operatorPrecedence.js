ecdf30963de5406535570c7e5744d64781c97eeb
ExploitEngineer <revilwhoami0098@gmail.com>
2025-06-29 08:13:29 +0500

    feat: update operatorPrecedence
    update some comments and code for better understanding

--- part1 (Basics)/04_operatorPrecedence.js
+++ part1 (Basics)/04_operatorPrecedence.js
@@ -1,8 +1,5 @@
-// operatorPrecedence.js
+/* INFO: Operator Precedence in JavaScript
 
-// INFO: Operator Precedence in JavaScript
-
-/*
 Operator precedence determines the order in which parts of an expression are evaluated.
 
 Expressions with multiple operators are evaluated based on this precedence,
@@ -21,16 +18,12 @@ let score = 2 * 3 + 2 - 1;
 Example 2: With parentheses (clear and recommended)
 let score1 = ((2 * (3 + 2)) - 1);
 // Evaluated as: 2 * (5) - 1 = 10 - 1 = 9
-
----
-
-Console outputs:
 */
 
 let score = 2 * 3 + 2 - 1;
-let score1 = ((2 * (3 + 2)) - 1);
+let score1 = 2 * (3 + 2) - 1;
 
-console.log(score);  // Output: 7
+console.log(score); // Output: 7
 console.log(score1); // Output: 9
 
 // TIP: Use parentheses to make your code easier to read and avoid unexpected results.
