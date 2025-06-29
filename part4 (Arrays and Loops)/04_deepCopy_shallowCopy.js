69076e95ab73d7515a3a176857b2a3d00fdd27bd
ExploitEngineer <revilwhoami0098@gmail.com>
2025-06-30 02:29:46 +0500

    feat: add deep clone

--- part4 (Arrays and Loops)/04_deepCopy_shallowCopy.js
+++ part4 (Arrays and Loops)/04_deepCopy_shallowCopy.js
@@ -9,10 +9,9 @@ console.log(myFruits.push("Dates"));
 console.log(fruits);
 console.log(myFruits);
 
-
 const user1 = {
-    firstName: "Abdul",
-    lastName: "rafay"
+  firstName: "Abdul",
+  lastName: "rafay",
 };
 const user2 = user1;
 
@@ -20,17 +19,16 @@ user2.firstName = "whoami";
 console.log(user2);
 console.log(user1);
 
-
 const username1 = "abdul rafay";
 let username2 = username1;
-username2 += " " + "whoami"
+username2 += " " + "whoami";
 
 console.log(username1);
 console.log(username2);
 
 const obj = {
-    name: "whoami",
-    age: 17
+  name: "whoami",
+  age: 17,
 };
 const obj2 = { ...obj };
 obj2.name = "nobody";
@@ -42,7 +40,6 @@ Object.assign(obj3, obj);
 obj3.age = 18;
 console.log(obj);
 console.log(obj3);
-
 
 const arr = ["whoami", "zain", "rafay"];
 
@@ -60,29 +57,32 @@ console.log(arr1);
 console.log(arr2);
 console.log(arr3);
 
-
 // FIXME: How to do a Deep Copy
 // INFO: There are two ways that i will gonna show you to do a deep copy
 const object1 = {
-    name: "rafay",
-    age: 25,
-    social: {
-        facebook: {
-            ac1: "someone@gmail.com",
-            ac2: "somewhere@gmail.com"
-        },
-        twitter: {
-            free: {
-                ac1: "somewhere@gmail.com"
-            },
-            paid: {
-                ac1: "paidaccount@gmail.com"
-            }
-        }
-    }
-}
+  name: "rafay",
+  age: 25,
+  social: {
+    facebook: {
+      ac1: "someone@gmail.com",
+      ac2: "somewhere@gmail.com",
+    },
+    twitter: {
+      free: {
+        ac1: "somewhere@gmail.com",
+      },
+      paid: {
+        ac1: "paidaccount@gmail.com",
+      },
+    },
+  },
+};
 
 const object2 = JSON.parse(JSON.stringify(object1));
 object2.social.facebook.ac1 = "changed";
 console.log(object1.social.facebook.ac1);
 console.log(object2.social.facebook.ac1);
+
+// Second method
+const clone = structuredClone(object1);
+console.log(clone);
