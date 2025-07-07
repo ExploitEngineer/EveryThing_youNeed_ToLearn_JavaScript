7eb026ff66e11ba82cfe4f5a10c5152469555739
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-07 16:56:01 +0500

    feat: add strict mode

---
+++ part9 (Advanced Topics)/25_strict_mode.js
@@ -0,0 +1,9 @@
+/*
+INFO: What is Strict Mode ?
+Strict Mode is a way to opt into a restricted version of javascript. It prevents you from doing things that are error-prone or poorly supported, and throws more errors when you write bad code.
+- Must appear at the top of a script or function
+- Applies only to the code in that scope (global or function)
+*/
+
+// You enable it with:
+"use strict";
